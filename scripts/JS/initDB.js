cb5f001c7cfb5b1f53aa58aec109861c9ca03515
yrhm00 <etu52816@henallux.be>
2025-11-12 18:06:38 +0100

    Add database initialization script to execute SQL commands

---
+++ scripts/JS/initDB.js
@@ -0,0 +1,15 @@
+import {readFileSync} from "node:fs";
+import {pool} from "../../database/database.js";
+
+const requests = readFileSync(
+    './scripts/SQL/initDB.sql',
+    {encoding: "utf-8"}
+);
+
+
+try {
+    await pool.query(requests, []);
+    console.log("done");
+} catch (e) {
+    console.error(e);
+}
