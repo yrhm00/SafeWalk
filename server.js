e47bd272225b335970fe5c411aaaeaf417159bed
yrhm00 <etu52816@henallux.be>
2025-11-13 23:38:18 +0100

    Add route handlers and error handling middleware in server.js

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 import express from "express";
+import userRoutes from "./routes/user.js";
+import reportRoutes from "./routes/report.js";
+import commentRoutes from "./routes/comment.js";
+import zoneRoutes from "./routes/zone.js";
+import voteRoutes from "./routes/vote.js";
+import reportTypeRoutes from "./routes/reportType.js";
+
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World !');
+    res.send('API SafeWalk');
+});
+
+app.use('/users', userRoutes);
+app.use('/reports', reportRoutes);
+app.use('/comments', commentRoutes);
+app.use('/zones', zoneRoutes);
+app.use('/votes', voteRoutes);
+app.use('/report-types', reportTypeRoutes);
+
+// Gestion simple 404
+app.use((req, res) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+// Gestion simple erreurs
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
 });
 
 app.listen(port, () => {
