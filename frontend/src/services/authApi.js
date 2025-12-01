ad45cb67a76cdd3d90a9cd6288a1771d147f4d61
yrhm00 <etu52816@henallux.be>
2025-12-01 15:18:31 +0100

    chore: Remove all frontend and mobile application code.

--- frontend/src/services/authApi.js
+++
@@ -1,6 +0,0 @@
-import apiClient from './apiClient.js';
-
-export async function login(credentials) {
-  const response = await apiClient.post('/users/login', credentials);
-  return response.data;
-}
--- mobile/src/config/apiConfig.js
+++
@@ -1,7 +0,0 @@
-// mobile/src/config/apiConfig.js
-import Constants from 'expo-constants';
-
-export function getApiBaseUrl() {
-  const extra = Constants.expoConfig?.extra || Constants.manifest?.extra || {};
-  return extra.apiBaseUrl;
-}
