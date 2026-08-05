b545eb2a12bf92dd21a8be1589ff11078f023d02
Camomii <hogasmanu@gmail.com>
2026-08-05 04:32:18 +0200

    refactor(backend): secure configuration, jwt, password hashing and middlewares

---
+++ backend/src/routes/index.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import userRoutes from "./user.js";
+import reportRoutes from "./report.js";
+import commentRoutes from "./comment.js";
+import zoneRoutes from "./zone.js";
+import voteRoutes from "./vote.js";
+import reportTypeRoutes from "./reportType.js";
+
+const router = Router();
+
+router.use("/users", userRoutes);
+router.use("/reports", reportRoutes);
+router.use("/comments", commentRoutes);
+router.use("/zones", zoneRoutes);
+router.use("/votes", voteRoutes);
+router.use("/report-types", reportTypeRoutes);
+
+export default router;
